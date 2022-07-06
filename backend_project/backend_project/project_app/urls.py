7707634b4a13d2463cb03cea56adb45fbac4d5a3
khsrla9806 <kimhunsope@gmail.com>
2022-07-06 21:16:37 +0900

    work 2 : Detail html + backendteam_intro.html " 팀원소개 html과 연결, detail 페이지(게시글, 기업) 만듬 "

--- backend_project/backend_project/project_app/urls.py
+++ backend_project/backend_project/project_app/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name = 'home'),
+    path('team_intro/', views.team_intro, name = 'team_intro'),
     path('post_list/', views.post_list, name = 'post_list'),
+    path('post_detail/', views.post_detail, name = 'post_detail'),
     path('company_list/', views.company_list, name = 'company_list'),
+    path('company_detail/', views.company_detail, name = 'company_detail'),
 ]
--- backend_project/backend_project/project_app/views.py
+++ backend_project/backend_project/project_app/views.py
@@ -3,8 +3,20 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'index.html')
 
+def team_intro(request):
+    return render(request, 'backend_team.html')
+
+
 def post_list(request):
     return render(request, 'post_list.html')
 
+def post_detail(request):
+    return render(request, 'post_detail.html')
+
 def company_list(request):
     return render(request, 'company_list.html')
+
+def company_detail(request):
+    return render(request, 'company_detail.html')
+
+
